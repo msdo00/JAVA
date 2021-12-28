d44f3f7e6975217b947515f12547e4b5edaa5c0e
admin <admin@YD5-13>
2021-12-28 09:05:19 +0900

    first

---
+++ hellojava/src/com/yedam/java/main/Hellojava.java
@@ -0,0 +1,12 @@
+package com.yedam.java.main;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// 한국
+		// 시스템에 출력하는 기능
+        System.out.println("Wellcome to JAVA");
+        
+	}
+
+}
